d7e12732f1789eb3e1fa9fc4dad528dfe225bd23
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-04-02 20:20:44 +0200

    commit-2
    Test exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,102 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import { Formik, Form } from 'formik';
 
-const LoginForm = () => {
-	const handleSubmit = (evt) => {
-		evt.preventDefault();
-		const form = evt.target;
-		const { login, password } = form.elements;
-		console.log(login, password);
-		console.log(login.value, password.value);
-		form.reset();
+//!======== КОМПОНЕНТ ФОРМА ===========================
+
+// const LoginForm = () => {
+// 	const handleSubmit = (evt) => {
+// 		evt.preventDefault();
+// 		const form = evt.target;
+// 		const { login, password } = form.elements;
+// 		console.log(login, password);
+// 		console.log(login.value, password.value);
+// 		form.reset();
+// 	};
+
+// 	return (
+// 		<form onSubmit={handleSubmit}>
+// 			<input type="text" name="login" />
+// 			<input type="password" name="password" />
+// 			<button type="submit">Login</button>
+// 		</form>
+// 	);
+// };
+
+// const LoginForm = ({ onLogin }) => {
+// 	const handleSubmit = (evt) => {
+// 		evt.preventDefault();
+
+// 		const form = evt.target;
+// 		const { login, password } = form.elements;
+
+// 		// Викликаємо пропс onLogin
+// 		onLogin({
+// 			login: login.value,
+// 			password: password.value,
+// 		});
+
+// 		form.reset();
+// 	};
+
+// 	return (
+// 		<form onSubmit={handleSubmit}>
+// 			<input type="text" name="login" />
+// 			<input type="password" name="password" />
+// 			<button type="submit">Login</button>
+// 		</form>
+// 	);
+// };
+
+//!======== КОМПОНЕНТ SearchBar ===========================
+const SearchBar = () => {
+	const [inputValue, setInputValue] = useState('');
+
+	const handleChange = (evt) => {
+		setInputValue(evt.target.value);
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input type="text" name="login" />
-			<input type="password" name="password" />
-			<button type="submit">Login</button>
-		</form>
+		<div>
+			<input type="text" value={inputValue} onChange={handleChange} />
+			<p>{inputValue}</p>
+		</div>
+	);
+};
+//!======== КОМПОНЕНТ АРР ===========================
+
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={reactLogo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>App.jsx</code> and save to reload.
+// 				</p>
+// 				<LoginForm />
+// 			</header>
+// 		</div>
+// 	);
+// }
+
+const App = () => {
+	// Колбек-функція для обробки сабміту форми
+	const handleLogin = (userData) => {
+		// Виконуємо необхідні операції з даними
+		console.log(userData);
+	};
+
+	return (
+		<div>
+			<h1>Please login to your account!</h1>
+			{/* Передаємо колбек як пропс форми */}
+			{/* <LoginForm onLogin={handleLogin} /> */}
+			<SearchBar />
+		</div>
 	);
 };
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={reactLogo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>App.jsx</code> and save to reload.
-				</p>
-				<LoginForm />
-			</header>
-		</div>
-	);
-}
-
 export default App;
+
+// 23527
+// 12197
+// 5682
